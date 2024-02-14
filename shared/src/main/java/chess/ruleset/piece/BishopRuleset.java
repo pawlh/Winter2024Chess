b0eb68d879fb5ca2d7e8e355596c235b74539c40
Michael Davenport <mdaven19@byu.edu>
2024-02-14 11:12:14 -0700

    fix bishop ruleset use hashset

--- shared/src/main/java/chess/ruleset/piece/BishopRuleset.java
+++ shared/src/main/java/chess/ruleset/piece/BishopRuleset.java
@@ -4,14 +4,14 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class BishopRuleset extends LineMoveRuleset {
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>();
+        Collection<ChessMove> moves = new HashSet<>();
 
         moves.addAll(lineMove(board, myPosition, 1, 1));
         moves.addAll(lineMove(board, myPosition, 1, -1));
